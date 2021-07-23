b10a4bd48606aee9673c419c43656b93bced5d3e
J05HYYY <lovell.joshyyy@gmail.com>
2021-07-23 11:15:38 +0000

    Build all .java files together. Rename our partial script to something else.

---
+++ reconstruct6.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+thepwd="$PWD"
+
+cd sources/structure
+
+find . -maxdepth 3 -mindepth 3 -type d | while read afile; do
+
+groupId="$(printf "%s" "${afile}" | cut -d "/" -f 2)"
+artifactId="$(printf "%s" "${afile}" | cut -d "/" -f 3)"
+version="$(printf "%s" "${afile}" | cut -d "/" -f 4)"
+
+	if [ -d ${groupId}/${artifactId}/${version} ]; then
+
+		mkdir -p "${thepwd}/fileListsAndDeps/${groupId}/${artifactId}/${version}"
+
+		if [ -d "${groupId}/${artifactId}/${version}/extractedSources" ]; then
+
+			find -L "${groupId}/${artifactId}/${version}/extractedSources" -name "*.java" | while read line; do
+				printf "%s\n" "$line" >> "${thepwd}/fileListsAndDeps/${groupId}/${artifactId}/${version}/${groupId}.${artifactId}_vanilla.fileList"
+			done
+		fi
+
+
+		if [ -d "${groupId}/${artifactId}/${version}/Decompiled" ]; then
+			find -L "${groupId}/${artifactId}/${version}/Decompiled" -name "*.java" | while read line; do
+				printf "%s\n" "$line" >> "${thepwd}/fileListsAndDeps/${groupId}/${artifactId}/${version}/${groupId}.${artifactId}.fileList"
+			done
+		fi
+
+
+	else
+		printf "%s does not exist!\n" "${groupId}/${artifactId}/${version}"
+	fi
+
+done
