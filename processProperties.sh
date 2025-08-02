60a85a57f3725ea7446d48df71b64a47923ee18d
publicsite <34146156+publicsite@users.noreply.github.com>
2025-08-02 16:54:38 +0100

    Update processProperties.sh

--- processProperties.sh
+++ processProperties.sh
@@ -6,6 +6,11 @@ fi
 thePom="$(cat "$1" | sed 's#\r##g' | sed 's#[ \t]##g' | tr -d "\n")"
 theProperties="$(printf "%s" "$thePom" | grep -o '<properties>.*</properties>')"
 
+if [ "$1" != "$2" ]; then
+	oneArtifactId="$(grep -o "<artifactId>.*</artifactId>" "$1"  | head -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1)"
+	sed -i "s#\${artifactId}#${oneArtifactId}#g" "$2"
+fi
+
 old_ifs="$IFS"
 IFS="<"
 
@@ -26,6 +31,6 @@ for i in $(seq $count); do
 	toprocess="$(echo "$listOfProperties" | head -n $i | tail -n 1)"
 	varname="$(echo $toprocess | cut -d '>' -f 1)"
 	varvalue="$(echo $toprocess | cut -d '>' -f 2)"
-	sed -i "s#\${${varname}}#${varvalue}#g" "$1"
+	sed -i "s#\${${varname}}#${varvalue}#g" "$2"
 	listOfProperties="$(echo "$listOfProperties" | sed "s#\${${varname}}#${varvalue}#g")"
 done
